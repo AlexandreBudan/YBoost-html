1138433ba199e714893244de561bbb626043f6e1
root <root@Destkop-030604.localdomain>
2022-11-23 21:32:38 +0100

    Modif

--- serv.go
+++ serv.go
@@ -12,12 +12,44 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/Sneakers", Sneakers)
+	http.HandleFunc("/Teams", Teams)
+	http.HandleFunc("/Players", Players)
+	http.HandleFunc("/About", About)
 	fmt.Println("http://localhost:8080")
 
 	http.ListenAndServe(":8080", nil)
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, _ := template.ParseFiles("./HomePage.html")
+
+	tmpl.Execute(w, "")
+}
+
+func Sneakers(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, _ := template.ParseFiles("./HomePage.html")
+
+	tmpl.Execute(w, "")
+}
+
+func Teams(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, _ := template.ParseFiles("./HomePage.html")
+
+	tmpl.Execute(w, "")
+}
+
+func Players(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, _ := template.ParseFiles("./HomePage.html")
+
+	tmpl.Execute(w, "")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, _ := template.ParseFiles("./HomePage.html")
 
